1c13f3010dc01d20a6a0e7053dc42ab00676ff39
Willian Silva <silva.williansantos@gmail.com>
2024-10-21 10:30:01 -0300

    feat: :sparkles: add sum all function

--- arrays/sum.go
+++ arrays/sum.go
@@ -9,3 +9,12 @@ func Sum(numbers []int) int {
 
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}

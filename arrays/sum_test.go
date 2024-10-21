1c25f1bbbc2d14de3652ddcd05abbdee274b1de6
Willian Silva <silva.williansantos@gmail.com>
2024-10-21 10:18:21 -0300

    feat: :sparkles: add sum slice function

---
+++ arrays/sum_test.go
@@ -0,0 +1,16 @@
+package arrays
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+
+		got := Sum(numbers)
+		expected := 15
+
+		if got != expected {
+			t.Errorf("got %d want %d given, %v", got, expected, numbers)
+		}
+	})
+}

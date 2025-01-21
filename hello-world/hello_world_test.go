c83c489165d782a00860ceb7f891955169ef161e
Willian Silva <silva.williansantos@gmail.com>
2025-01-21 14:11:35 -0300

    feat: add helper function for test assertion

--- hello-world/hello_world_test.go
+++ hello-world/hello_world_test.go
@@ -7,17 +7,24 @@ func TestHello(t *testing.T) {
 		got := Hello("Willian")
 		want := "Hello, Willian!"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World!' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World!"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	// testing.TB is an interface that is available in the testing package and
+	// and means that we can pass in anything that satisfies the testing.TB interface.
+	// In this case, we pass in *testing.T.
+	// t.Helper() tells the test suite that this method is a helper.
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }

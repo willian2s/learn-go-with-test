d0a9af374194ddf6622daf8d2a595c8126ef4eae
Willian Silva <silva.williansantos@gmail.com>
2025-01-21 14:07:08 -0300

    feat: add english prefix message and default value to Hello function

--- hello-world/hello_world.go
+++ hello-world/hello_world.go
@@ -2,8 +2,14 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello"
+
 func Hello(name string) string {
-	return fmt.Sprintf("Hello, %s!", name)
+	if name == "" {
+		return englishHelloPrefix + ", World!"
+	}
+
+	return fmt.Sprintf("%s, %s!", englishHelloPrefix, name)
 }
 
 func main() {

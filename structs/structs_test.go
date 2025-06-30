95c80c5a94847707ee1bf75f59243891398afff9
Willian Silva <silva.williansantos@gmail.com>
2025-06-30 18:36:48 -0300

    add structs

---
+++ structs/structs_test.go
@@ -0,0 +1,71 @@
+package structs
+
+import (
+	"math"
+	"testing"
+)
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+type Shape interface {
+	Area() float64
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
+
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+	areaTests := []struct {
+		shape   Shape
+		hasArea float64
+		title   string
+	}{
+		{shape: Rectangle{12, 6}, hasArea: 72.0, title: "Rectangle"},
+		{shape: Circle{10}, hasArea: 314.1592653589793, title: "Circle"},
+		{shape: Triangle{12, 6}, hasArea: 36.0, title: "Triangle"},
+	}
+
+	for _, tt := range areaTests {
+		t.Run(tt.title, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
+	}
+}

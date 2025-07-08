ee3e26f1898c39536849627dcc9c35be319d57dd
Willian Silva <silva.williansantos@gmail.com>
2025-07-08 14:55:57 -0300

    refactoring DI

--- di/di.go
+++ di/di.go
@@ -1,10 +1,20 @@
-package di
+package main
 
 import (
 	"fmt"
 	"io"
+	"log"
+	"net/http"
 )
 
 func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "word")
+}
+
+func main() {
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
 }

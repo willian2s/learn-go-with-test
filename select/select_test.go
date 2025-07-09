ff2014a50bf3e9cf73b842caaa5c10bdc710642f
Willian Silva <silva.williansantos@gmail.com>
2025-07-09 11:18:33 -0300

    add select

---
+++ select/select_test.go
@@ -0,0 +1,47 @@
+package racer
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func mockServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+}
+
+func TestRacer(t *testing.T) {
+	t.Run("compare speeds of servers, returning the url of the fastest one", func(t *testing.T) {
+		slowServer := mockServer(20 * time.Millisecond)
+		fastServer := mockServer(0 * time.Millisecond)
+
+		defer slowServer.Close()
+		defer fastServer.Close()
+
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
+
+		want := fastURL
+		got, _ := Racer(slowURL, fastURL)
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("returns an error if a server doesn't respond within the specified time", func(t *testing.T) {
+		server := mockServer(25 * time.Millisecond)
+
+		defer server.Close()
+
+		_, err := ConfigurableRacer(server.URL, server.URL, 20*time.Millisecond)
+
+		if err == nil {
+			t.Error("expected an error but didn't get one")
+		}
+	})
+}
